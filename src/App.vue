54de5d69b3ac6dc6742b75b1f7297c5d6117ebdf
anythingelse428 <vladn803@gmail.com>
2021-07-20 03:39:06 +0400

    0

--- src/App.vue
+++ src/App.vue
@@ -6,7 +6,17 @@
 </template>
 
 <style>
+:root{
+  --letterHov:#7e7cec;
+  --letterBg1:#ef7b79;
+  --letterBg2:#edef79;
+  --letterBg3:#79edef;
+}
 #app {
+    display: flex;
+    flex-direction: column;
+    align-content: center;
+    align-items: center;
   font-family: Avenir, Helvetica, Arial, sans-serif;
   -webkit-font-smoothing: antialiased;
   -moz-osx-font-smoothing: grayscale;
--- src/views/Home.vue
+++ src/views/Home.vue
@@ -18,133 +18,43 @@
    </div>
    <div class="typing ">
        
-  <textarea @input="changeButtonColor(),test()" @keypress.once="timerStart()"  v-model="UserInput" class="form-control border-dark" id="floatingTextarea"></textarea>
+  <textarea @input="test()" @keypress.once="timerStart()"  v-model="UserInput" class="form-control border-dark" id="floatingTextarea"></textarea>
 
    </div>
  
-<div class="qwerty" v-if="lang=='RU'">
-  
-<div class="line instrumental">
 
-    <div class="btn-group dropend ">
-    <button class="btn btn-secondary dropdown-toggle" type="button" id="dropdownMenuButton1" data-bs-toggle="dropdown" aria-expanded="false">
-     <i class="bi bi-volume-up"></i>
-    </button>
-    <ul class="dropdown-menu" aria-labelledby="dropdownMenuButton1">
-      <li><a class="dropdown-item" href="#" @click="choosnSound='Iphone'">Iphone sound</a></li>
-      <li><a class="dropdown-item" href="#" @click="choosnSound='Mech'">Mech sound</a></li>
-      <li><a class="dropdown-item" href="#" @click="choosnSound=''">No sound</a></li>
-      <li><input v-model="choosnSoundVol" type="range" id="volume" name="volume"
-         min="0" max="1" step=".01"></li>
-    </ul>
+<KeyBoard :UserInput="UserInput"/>
   </div>
-
-    <div class="btn-group dropend">
-    <button class="btn btn-secondary dropdown-toggle" type="button" id="dropdownMenuButton2" data-bs-toggle="dropdown"  aria-expanded="false">
-      <i class="bi bi-palette"></i>
-    </button>
-    <ul class="dropdown-menu" aria-labelledby="dropdownMenuButton2">
-    <li class="dropdown-item"><input type="color" @input="cssRootAdd()" v-model="BGcolor1"/> Цвет кнопок на первой линии</li>
-    <li class="dropdown-item"><input type="color" @input="cssRootAdd()" v-model="BGcolor2"/> Цвет кнопок на второй линии</li>
-    <li class="dropdown-item"><input type="color" @input="cssRootAdd()" v-model="BGcolor3"/> Цвет кнопок на третьей линии</li>
-    <li class="droprown-item"><input type="color" @input="cssRootAdd()" v-model="hoverCLR"/> Цвет активации кнопок</li>
-    </ul>
-  </div>
-
-</div>
-   <div class="line" id="line1">
-     <span  class="text-dark letter border rounded-circle" :id="ruButtons1" v-for="ruButtons1 in QWERTYru1" :key="ruButtons1.QWERTYru1">
-       {{ruButtons1}}
-       </span>
-       </div>
-       <br>
-   <div class="line" id="line2">
-     <span  class="text-dark letter border rounded-circle" :id="ruButtons2" v-for="ruButtons2 in QWERTYru2" :key="ruButtons2.QWERTYru2">
-       {{ruButtons2}}
-       </span>
-       </div>
-       <br>
-   <div class="line" id="line3">
-     <img @click="switchLang()" src="../assets/Language.png" alt="" srcset="" width="60" class="langBTN">
-
-     <span class="text-dark letter border rounded-circle" :id="ruButtons3" v-for="ruButtons3 in QWERTYru3" :key="ruButtons3.QWERTYru3">
-       {{ruButtons3}}
-       </span>
-       </div>
-
-       <div class="spaceBtn flex letter  border" id=" "></div>
- </div>
-
-
- <div class="qwerty" v-if="lang=='EN'" >
-   <div class="line" id="1">
-     <span  class="text-dark letter border rounded-circle" :id="enButtons1" v-for="enButtons1 in QWERTYen1" :key="enButtons1.QWERTYen1">
-       {{enButtons1}}
-       </span>
-       </div>
-       <br>
-   <div class="line" id="2">
-     <span class="text-dark letter border rounded-circle" :id="enButtons2" v-for="enButtons2 in QWERTYen2" :key="enButtons2.QWERTYen2">
-       {{enButtons2}}
-       </span>
-       </div>
-       <br>
-   <div class="line" id="3">
-     <img @click="switchLang()" src="../assets/Language.png" alt="" srcset="" width="60" class="langBTN">
-
-     <span  class="text-dark letter border rounded-circle" :id="enButtons3" v-for="enButtons3 in QWERTYen3" :key="enButtons3.QWERTYen3">
-       {{enButtons3}}
-       </span>
-       </div>
- </div>
-  </div>
-
 
 </template>
 
 <script>
 
-import {Howl} from 'howler';
-
+// import {Howl} from 'howler';
+import KeyBoard from '../components/KeyBoard.vue'
 export default {
   name: 'Home',
-
+components:{
+  KeyBoard
+},
   data:function(){
     return{
-      root: null,
-      hoverCLR:'#3232FA',
-      BGcolor1:'#ffffff',
-      BGcolor2:'#ffffff',
-      BGcolor3:'#ffffff',
+      lang:'RU',
       time:0,
       timePretty:'0.00',
-      lang:'RU',
-      choosnSound:'Iphone',
-      choosnSoundVol:.7,
-      QWERTYen1: ['q', 'w', 'e', 'r', 't', 'y', 'u', 'i', 'o', 'p'],QWERTYen2:['a', 's', 'd', 'f', 'g', 'h', 'j', 'k', 'l'],QWERTYen3:['z', 'x', 'c', 'v', 'b', 'n', 'm'],
-      QWERTYru1: ['й', 'ц', 'у', 'к', 'е', 'н', 'г', 'ш', 'щ', 'з', 'х', 'ъ'], QWERTYru2:['ф', 'ы', 'в', 'а', 'п', 'р', 'о', 'л', 'д', 'ж'], QWERTYru3:['э', 'я', 'ч', 'с','м','и','т','ь','б','ю'],
        textForTypeEN:["Lorem ipsum dolor sit amet consectetur adipisicing elit. Nemo quae inventore, nam, impedit ad veritatis neque ex atque perspiciatis quo, dicta cupiditate! Odit perferendis libero esse et accusantium, commodi vero?"],
        textForTypeRU:["Таким образом начало повседневной работы по формированию позиции играет важную роль в формировании соответствующий условий активизации"],
        ready:0,
        UserInput:'',
     }
   },
-  mounted: function() {
-    this.root = document.documentElement;
-  },
+ 
  
   methods:{
     change (val) {
       console.log('change', val)
     },
-cssRootAdd: function() {
-        this.$nextTick(() => {
-            this.root.style.setProperty("--letterHov", this.hoverCLR);
-            this.root.style.setProperty("--letterBg1", this.BGcolor1);
-            this.root.style.setProperty("--letterBg2", this.BGcolor2);
-            this.root.style.setProperty("--letterBg3", this.BGcolor3);
-        })
-      },
+     
     test(){
       function replaceAll(string, search, replace) {
         return string.toLowerCase().split(search.toLowerCase()).join(replace); 
@@ -168,75 +78,6 @@ cssRootAdd: function() {
         }
         
     },
-
-    changeButtonColor(){   
-      let currentBTN = this.UserInput[this.UserInput.length-1]
-      let beforeStyle = document.getElementById(currentBTN.toLowerCase()).style.cssText
-            document.getElementById(currentBTN.toLowerCase()).style.cssText='background-color:var(--letterHov)!important;top: 1px; left: 1px;position: relative;'
-      setTimeout(() => {
-            document.getElementById(currentBTN.toLowerCase()).style.cssText=beforeStyle
-            return
-          }, 200)
-
-      let sound1 = new Howl({
-        src: [require('../assets/1.mp3')],
-        volume: this.choosnSoundVol,
-      })
-      let sound2 = new Howl({
-        src: [require('../assets/2.mp3')],
-        volume: this.choosnSoundVol,
-      })
-      let arrSound=[sound1,sound2]
-      let MechSnd = new Howl({
-        src: [require('../assets/click.mp3')],
-        volume: this.choosnSoundVol,
-      })
-
-        
-        
- 
-              // start shiting 4 iphone sound
-      function getRandomInt(max) {
-            return Math.floor(Math.random() * max)
-               }
-      function playing(){
-            let a = getRandomInt(2)
-            arrSound[a].play()
-            return
-        }
-              // end shiting 4 iphone sound
-            
-
-      switch (this.choosnSound){
-        case 'Iphone':{
-            
-            playing()
-            break
-        }
-        case 'Mech':{
-          MechSnd.play()
-        break
-        }
-      }
-      
-// симпл мод: охуенно красивыо анимированные аки воллпапер жнжин фоны и простая кастомизируемая коавиатура 
-// фоный можно крутить слайдером с желли эфектом
-        
-   
-      
-    },
-
-    switchLang(){
-      switch(this.lang){
-        case 'RU':
-          this.lang='EN'
-          break
-        default:
-          this.lang='RU'
-      }
-      console.log(this.lang)
-    },
-
   timerStart(){
     let starttime = Date.now()
     setInterval(() => {
@@ -254,7 +95,8 @@ cssRootAdd: function() {
       }, 1)
       
          
-  }
+  },
+  
   },
 
   beforeUpdate: function(){
@@ -275,20 +117,11 @@ cssRootAdd: function() {
 </script>
 <style scoped>
 @import url("https://cdn.jsdelivr.net/npm/bootstrap-icons@1.5.0/font/bootstrap-icons.css");
- @import url('https://fonts.googleapis.com/css2?family=Tourney:ital,wght@0,400;1,100;1,800;1,900&display=swap');
-
- 
+@import url('https://fonts.googleapis.com/css2?family=Tourney:ital,wght@0,400;1,100;1,800;1,900&display=swap');
    textarea{
      width: 100%;
      height: 100%;
      resize: none;
-   }
-   .instrumental{
-     margin: 1em auto;
-   }
-   .chooseSnd{
-     position: relative;
-     right: 5vw;
    }
   .timer{
     font-weight: 900;
@@ -301,72 +134,8 @@ cssRootAdd: function() {
         font-family: 'Comfortaa', cursive;
         text-transform: uppercase;
       }
-  .qwerty {
-    position: relative;
-    display: flex;
-    margin: 0 25vw;
-    width: 50vw;
-    flex-direction: column;
-    justify-content: space-between;
-    flex-wrap: wrap;
-}
-.line {
-    display: inline-flex;
-    flex-direction: row;
-    flex-wrap: nowrap;
-    justify-content: center;
-    align-items: center;
-} 
-#line1 span{
-background-color: var(--letterBg1);
-}
-#line2 span{
-background-color: var(--letterBg2);
-}
-#line3 span{
-background-color: var(--letterBg3);
-}
-  .letter {
-    width: 1.5em;
-    height: 1.5em;
-    font-family: 'Roboto', sans-serif;
-    font-size: 4rem;
-    font-weight: 100;
-    margin: 0 .5% .5% 0;
-    line-height: 1.3em;
-    text-align: center;
-    -moz-border-radius: 5px;
-    -webkit-border-radius: 5px;
-    border: 1px solid #dee2e6;
-    
-}
-      .letter:hover {
-        position: relative;
-        top: 1px;
-        left: 1px;
-        border-color: #e5e5e5;
-        background-color: var(--letterHov)!important;
-        cursor: pointer;
-        }
-
-  .langBTN{
-    cursor: pointer;
-    margin-right: 1%;
-  }
-  .langBTN:hover{
-    filter: contrast(10);
-  }
-  .spaceBtn{
-    width: 90%;
-    height: 6vh;
-    margin: 0 auto;
-  }
-  .blue{
-    color: rgb(77, 35, 228)!important
-  }
-  .red{
-    color: rgb(250, 14, 14)!important;
-  }
+ 
+ 
   .home{
     font-family: 'Comfortaa', cursive;
     width: 75vw;
@@ -379,7 +148,6 @@ background-color: var(--letterBg3);
     width: 100%;
     position: relative;
     z-index: 1;
-    margin: 0 25%;
     font-size: 1.666em;
     color: black;
   }
@@ -396,12 +164,10 @@ background-color: var(--letterBg3);
       flex-wrap: wrap;
       align-items: center;
   }
-
-
   .typing {
       display: flex;
       position: relative;
-      margin: 1% 25%;
+
       width: 100%;
       height: 10vh;
   }
@@ -409,6 +175,44 @@ background-color: var(--letterBg3);
     width: 100%;
     height: 100%;
   }
+
+   @media screen  and (max-width: 600px){
+    .isUserReady {
+    font-size: 1em;
+    height: 100%;
+    width: 100%;
+    z-index: 10;
+    position: absolute;
+    background: rgba(64, 6, 199, 0.95);
+    display: flex;
+    justify-content: space-around;
+    align-content: space-around;
+    flex-wrap: wrap;
+    align-items: center;
+}
+     .textContainer {
+    width: 100%;
+    position: relative;
+    z-index: 1;
+    font-size: 0.777em;
+    color: black;
+}
+ }
+ @media screen  and (max-width: 768px) and (min-width: 600px){
+     .isUserReady {
+    font-size: 1.337em;
+    height: 100%;
+    width: 100%;
+    z-index: 10;
+    position: absolute;
+    background: rgba(64, 6, 199, 0.95);
+    display: flex;
+    justify-content: space-around;
+    align-content: space-around;
+    flex-wrap: wrap;
+    align-items: center;
+}
+ }
 </style>>
 
   
